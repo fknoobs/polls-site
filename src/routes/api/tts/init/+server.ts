433098b99d062dd9f41359630d1d7947e6c1f3fc
codeit-ninja <richard@codeit.ninja>
2024-07-30 10:05:38 +0200

    Add adolf voice

--- src/routes/api/tts/init/+server.ts
+++ src/routes/api/tts/init/+server.ts
@@ -1,6 +1,5 @@
 import { readFileSync } from "fs"
 import { json } from "@sveltejs/kit"
-import { Readable } from "stream"
 
 export const GET = async ({ locals }) => {
     const data = await locals.elevenlabs.voices.getAll()
@@ -12,6 +11,18 @@ export const GET = async ({ locals }) => {
         const file = `static/tts-audio-voices/statement/voice_of_statement_out_${(i + 1)}.wav`
         return new Blob([readFileSync(file)])
     })
+
+    /**
+     * Add Adolfs voice
+     */
+    if (!data.voices.find(voice => voice.name === 'Adolf')) {
+        await locals.elevenlabs.voices.add({
+            files: [new Blob([readFileSync('static/tts-audio-voices/adolf/voice_of_adolf.mp3')])],
+            name: 'Adolf'
+        })
+
+        console.log('Adolf has been added')
+    }
 
     /**
      * Add Sopa's voice
@@ -37,9 +48,23 @@ export const GET = async ({ locals }) => {
         console.log('Statement has been added')
     }
 
+    /**
+     * Add Annoying voice
+     */
+    if (!data.voices.find(voice => voice.name === 'Annoying')) {
+        await locals.elevenlabs.voices.addSharingVoice('1c07ef5740387faa0fd1f7e624ce51055c0b270674ab3da0f0e22c2882bce3c4', 'dfZGXKiIzjizWtJ0NgPy', {
+            new_name: 'Annoying'
+        })
+
+        console.log('Annoying has been added')
+    }
+
     return json({
         status: 200,
         statusText: 'READY',
-        data: data.voices.filter(voice => voice.name === 'Sopa')
+        data: await locals.elevenlabs.voices.getShared({
+            gender: 'male',
+            search: 'A male American comic and cartoon character.'
+        })
     })
 }
