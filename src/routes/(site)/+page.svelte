41a7a888d67b167b5cc564ad8652480ff96a6b65
codeit-ninja <richard@codeit.ninja>
2024-08-02 22:17:23 +0200

    Add tourney link to homepage

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -1,10 +1,20 @@
 <script>
     import Box from "$lib/components/Box.svelte";
+    import Button from "$lib/components/Button.svelte";
     import { classNames } from "$lib/utils";
     import Twitch from "lucide-svelte/icons/twitch";
+    import MoveRight from "lucide-svelte/icons/move-right";
 </script>
-<Box title="FkNoobsCoH" description="Welcome, there is not much to see here yet :)">
+<Box title="FkNoobsCoH">
     <section>
+        <div class="mb-8">
+            <a href="/tourneys" class="py-2 px-4 inline-block font-bold transition-colors bg-primary-200 hover:bg-primary-100">
+                <span class="flex items-center gap-2">
+                    Tournaments
+                    <MoveRight />
+                </span>
+            </a>
+        </div>
         <div class="flex items-center gap-3">
             <Twitch /> 
             <a href="https://twitch.ts/fknoobscoh" target="_blank" class="text-primary-700">https://twitch.ts/fknoobscoh</a>
